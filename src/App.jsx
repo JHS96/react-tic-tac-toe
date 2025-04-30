f85bac42f66b89aaaa89541b5bbb8ceb91595028
JHS96 <jschaper@hotmail.co.za>
2025-04-30 15:53:19 +0200

    GameBoard started

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Player from './components/Player';
+import GameBoard from './components/GameBoard';
 
 function App() {
   return (
@@ -6,9 +7,9 @@ function App() {
       <div id='game-container'>
         <ol id='players'>
           <Player initialName='Player 1' symbol='X' />
-          <Player initailName='Player 2' symbol='O' />
+          <Player initialName='Player 2' symbol='O' />
         </ol>
-        GAME BOARD
+        <GameBoard />
       </div>
       LOG
     </main>
